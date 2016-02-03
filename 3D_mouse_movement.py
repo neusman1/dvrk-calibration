adcf918afce611cb9a27129447d949e46e9e5c3f
neusman1 <nick.eusman@gmail.com>
2016-02-03 14:30:27 -0500

    Values now saved to csv file with both cartesian and joint positions

--- 3D_mouse_movement.py
+++ 3D_mouse_movement.py
@@ -1,10 +1,16 @@
+#created on january 5th 2016
+
+#to do:
+# -get joint position values
+# -write all values to a comma separated value file
+
 import sys
 from dvrk.arm import * 
 from sensor_msgs.msg import Joy
 import rospy
 import time
 import random
-
+import csv
 
 class calibration_testing:
 
@@ -13,6 +19,7 @@ class calibration_testing:
         self._robot = arm(self._robot_name)
         self._last_axes = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self._last_buttons = [0, 0]
+        self.previous_mouse_buttons = [0, 0]
         rospy.Subscriber('/spacenav/joy', Joy, self.joy_callback)
         
 
@@ -34,37 +41,53 @@ class calibration_testing:
         joint_orientation3 = random.uniform(-1.39626, 1.39626)
         self._robot.move_joint_list([joint_orientation1,joint_orientation2,joint_orientation3,0.0],[3,4,5,6])
 
-        move_mode = 0
+        joint_positions = []
+        cartesian_positions = []
+        sample_nb = 0
+        acceleration_counter = 1.0
         slow_speed = False
-        while move_mode < 10:
-            if slow_speed == False:  
-                scale = 1.0 / 500.0
-            elif slow_speed == True:
-                scale = 1.0 / 5000.0
+        while sample_nb < 10:
+            if self._last_axes[0] != 0 or self._last_axes[1] != 0 or self._last_axes[2] != 0:
+                acceleration_counter += 0.03
+            else:
+                acceleration_counter = 1.0
+            scale = acceleration_counter / 5000.0
             x = self._last_axes[0] * scale
             y = self._last_axes[1] * scale
             z = self._last_axes[2] * scale
-            # print x,y,z
             self._robot.delta_move_cartesian_translation([y, -x, z], False)
-            if self.mouse_buttons()[0] == 1:
+            if self.mouse_buttons()[0] == 1 and self.previous_mouse_buttons[0] == 0:
                 joint_orientation1 = random.uniform(-4.53786, 4.53786)
                 joint_orientation2 = random.uniform(-1.39626, 1.39626)
                 joint_orientation3 = random.uniform(-1.39626, 1.39626)
                 self._robot.delta_move_cartesian_translation([0.0,0.0,0.01])
                 self._robot.move_joint_list([joint_orientation1,joint_orientation2,joint_orientation3],[3,4,5])
-                move_mode += 1
-                print self._robot.get_current_cartesian_position().p
-            if self.mouse_buttons()[1] == 1:
-                slow_speed = not slow_speed
-                time.sleep(.5)
-                print "Slow speed is", slow_speed
+                cartesian_positions.append([self._robot.get_current_cartesian_position().p[0],self._robot.get_current_cartesian_position().p[1],self._robot.get_current_cartesian_position().p[2]])
+                joint_positions.append([self._robot.get_current_joint_position()[0],self._robot.get_current_joint_position()[1],self._robot.get_current_joint_position()[2],self._robot.get_current_joint_position()[3],self._robot.get_current_joint_position()[4],self._robot.get_current_joint_position()[5],self._robot.get_current_joint_position()[6]])
+                #print'C:  ', cartesian_positions
+                #print'J:  ', joint_positions
+                print cartesian_positions[sample_nb]
+                sample_nb += 1
+            
             """
-            if self.mouse_buttons()[1] == 1:
+            if self.mouse_buttons()[1] == 1 and self.previous_mouse_buttons[1] == 0:
                 self.open_jaw()
+            if self.mouse_buttons()[1] == 0 and self.previous_mouse_buttons[1] == 1:
                 self.close_jaw()
             """
-            time.sleep(0.01) # 0.01 is 10 ms
-            
+            self.previous_mouse_buttons[:] = self.mouse_buttons()
+            time.sleep(0.03) # 0.03 is 30 ms, which is the spacenav's highest output frequency
+
+        # write all values to csv file
+        csv_file_name = 'mouse_positions.csv'
+        print "Values will be saved in: ", csv_file_name
+        f = open(csv_file_name, 'wb')
+        writer = csv.writer(f)
+        writer.writerow(["cartesian positions"," "," ","joint positions"])
+        for i in range(10):
+            writer.writerow(cartesian_positions[i] + joint_positions[i])
+
+
             
 
 if (len(sys.argv) != 2):
