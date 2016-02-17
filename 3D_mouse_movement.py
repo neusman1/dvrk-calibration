7f1a6d99c0ddb2d680453d43a42bbeb525b88884
neusman1 <nick.eusman@gmail.com>
2016-02-17 16:16:16 -0500

    Uses sequence of joint positions instead of random numbers, secondary move to approximate position not working yet

--- 3D_mouse_movement.py
+++ 3D_mouse_movement.py
@@ -1,8 +1,9 @@
 #created on January 5th 2016
 
 #to do:
-#
-#
+# 27 positions
+# 3 for shaft, 3 for wrist, 3 fingers
+# after button pressed, move close to average cartesian position of previous trials
 
 import sys
 from dvrk.arm import * 
@@ -34,32 +35,16 @@ class calibration_testing:
         return self._last_buttons
     
     def run(self):
-        joint_positions = []
-        cartesian_positions = []
+        recorded_joint_positions = []
+        recorded_cartesian_positions = []
         sample_nb = 0
         acceleration_counter = 1.0
-
-        self._robot.move_joint_list([0.0,0.0,0.1],[0,1,2])
-        random.seed()
-        joint_orientation1 = random.uniform(-4.53786, 4.53786)
-        joint_orientation2 = random.uniform(-1.39626, 1.39626)
-        joint_orientation3 = random.uniform(-1.39626, 1.39626)
-        self._robot.move_joint_list([joint_orientation1,joint_orientation2,joint_orientation3,0.0],[3,4,5,6])
-
-        """
-        self._robot.move_joint_list([0.0,0.0,0.0,0.0],[3,4,5,6])
         range_of_motion = [ [-4.53786, 4.53786], [-1.39626, 1.39626], [-1.39626, 1.39626]]
-        move_amounts = []
-        joint_orientations = []
-        for i in range(3):
-            move_amounts.append(( range_of_motion[i][(1)] - range_of_motion[i][0] ) / sample_nb)
-        for i in range(3):
-            joint_orintations.append(range_of_motion[i][0] + ( move_amounts[i] * sample_nb )
-        self._robot.move_joint_list([joint_orientations[0],joint_orientations[1],joint_orientation[2],0.0],[3,4,5,6])
+        density = 3
+        joint_motions = [ 0, 0, 0]
+        joint_indexs = [ 0, 0, 0]
         
-        """
-
-        while sample_nb < 10:
+        while sample_nb < (density ** 3):
             if self._last_axes[0] != 0 or self._last_axes[1] != 0 or self._last_axes[2] != 0:
                 acceleration_counter += 0.03
             else:
@@ -70,17 +55,32 @@ class calibration_testing:
             z = self._last_axes[2] * scale
             self._robot.delta_move_cartesian_translation([y, -x, z], False)
             if self.mouse_buttons()[0] == 1 and self.previous_mouse_buttons[0] == 0:
-                joint_orientation1 = random.uniform(-4.53786, 4.53786)
-                joint_orientation2 = random.uniform(-1.39626, 1.39626)
-                joint_orientation3 = random.uniform(-1.39626, 1.39626)
-                self._robot.delta_move_cartesian_translation([0.0,0.0,0.01])
-                self._robot.move_joint_list([joint_orientation1,joint_orientation2,joint_orientation3],[3,4,5])
-                cartesian_positions.append([self._robot.get_current_cartesian_position().p[0],self._robot.get_current_cartesian_position().p[1],self._robot.get_current_cartesian_position().p[2]])
-                joint_positions.append([self._robot.get_current_joint_position()[0],self._robot.get_current_joint_position()[1],self._robot.get_current_joint_position()[2],self._robot.get_current_joint_position()[3],self._robot.get_current_joint_position()[4],self._robot.get_current_joint_position()[5],self._robot.get_current_joint_position()[6]])
-                #print'C:  ', cartesian_positions
-                #print'J:  ', joint_positions
-                print cartesian_positions[sample_nb]
+                #record data
+                recorded_cartesian_positions.append([self._robot.get_current_cartesian_position().p[0],self._robot.get_current_cartesian_position().p[1],self._robot.get_current_cartesian_position().p[2]])
+                recorded_joint_positions.append([self._robot.get_current_joint_position()[0],self._robot.get_current_joint_position()[1],self._robot.get_current_joint_position()[2],self._robot.get_current_joint_position()[3],self._robot.get_current_joint_position()[4],self._robot.get_current_joint_position()[5],self._robot.get_current_joint_position()[6]])
+                print recorded_cartesian_positions[sample_nb]
+                #calculate next joint position
+                shaft_remainder = sample_nb % (density ** 2)
+                shaft_index = (sample_nb - shaft_remainder) / (density ** 2)
+                finger_index = shaft_remainder % density
+                wrist_index = (shaft_remainder - finger_index) / density
+                joint_indexs = [shaft_index, wrist_index, finger_index]
+                for joint in range(3):
+                    joint_motions[joint] = range_of_motion[joint][0] + (joint_indexs[joint]*((range_of_motion[joint][1] - range_of_motion[joint][0])/(density -1)))
+                #move to next joint position
+                self._robot.delta_move_cartesian_translation([0.0,0.0,0.05])
+                self._robot.move_joint_list([joint_motions[0], joint_motions[1], joint_motions[2], 0.0],[3,4,5,6])
+                #move close to next cartesian position
+                cartesian_totals = []
+                average_cartesian_positions = []
+                for axis in range(3):
+                    for cartesian_sample in range(len(recorded_cartesian_positions)):
+                        cartesian_totals.append(recorded_cartesian_positions[cartesian_sample][axis])
+                    average_cartesian_positions.append(sum(cartesian_totals)/len(cartesian_totals))
+                    cartesian_totals = []
+                self._robot.move_cartesian_translation([average_cartesian_positions[0], average_cartesian_positions[1], average_cartesian_positions[2]+ 0.02])
                 sample_nb += 1
+                print "you are on sample: ", sample_nb, " / ", density**3
             
             """
             if self.mouse_buttons()[1] == 1 and self.previous_mouse_buttons[1] == 0:
