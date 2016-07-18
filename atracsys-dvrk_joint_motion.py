80ade6d32f9776055457d0ea678e358c1edbce22
neusman1 <neusman1@lcsr-adeguet1-b.win.ad.jhu.edu>
2016-07-18 15:40:52 -0400

    added some new UI features and updated to do list

--- atracsys-dvrk_joint_motion.py
+++ atracsys-dvrk_joint_motion.py
@@ -4,10 +4,11 @@
 #Author: Nick Eusman
 
 #To Do:
-# -csv file writing of values
+# -csv file writing of values (needs testing)
 # -start other code to do nmr registartion? (or maybe use same code?)
-# -run all the way through to test up/down mechanic
-# -check if getting atracsys values at all points
+# -run all the way through to test up/down mechanic (up down works, still need to run though)
+# -check if getting atracsys values at all points (ties into next)
+# -check if list of atracsys points is of size 0, then skip point (should be working, needs additional testing)
 
 import sys
 from dvrk.psm import * 
@@ -43,16 +44,38 @@ class calibration_testing:
         #stay running untill shutdown
         while not rospy.is_shutdown():
             self._robot.move_joint(numpy.array([0.0,0.0,0.1,0.0,0.0,0.0,-0.20]))
+            raw_input("The dvrk arm is about to make LARGE MOVEMENTS, please ensure there is ample space, then hit [enter]")
+            print "The robot will now move to its joint maxima and minima to ensure the atracsys has full vision"
+            time.sleep(2)
+            self._robot.move_joint(numpy.array([-70*d2r, -40*d2r, 0.055,0.0,0.0,0.0,-0.20]))
+            if len(self._points) == 0:
+                print "The Atracsys cannot see the minima joint position, please adjust the dvrk arm"
+                rospy.signal_shutdown('Atracsys cannot see minima')
+            time.sleep(1)
+            self._robot.move_joint(numpy.array([70*d2r, 40*d2r, 0.235,0.0,0.0,0.0,-0.20]))
+            if len(self._points) == 0:
+                print "The Atracsys cannot see the maxima joint position, please adjust the dvrk arm"
+                rospy.signal_shutdown('Atracsys cannot see maxima')
+            time.sleep(1)
+            
+            print "Begining test of each joint position"
+            global progress
+            global points_skipped
+            points_skipped = 0
+            progress = 0.0
+            
             time.sleep(1)
             #try density number of positions per joint in all possible configurations
             for axis0 in range(density):
                 for axis1 in range(density):
                     for axis2 in range(density):
                         for axis3 in range(density):
+                            sys.stdout.write('\rProgress %02.3f%%' %( progress / (3**6) *100))
+                            sys.stdout.flush()
                             for axis4 in range(density):
                                 for axis5 in range(density):
                                     joint_indexs = [axis0, axis1, axis2, axis3, axis4, axis5]
-                                    
+                                    progress = progress + 1
                                     for joint in range(6):
                                         if up_or_down[joint] == True:
                                             joint_motions[joint] = range_of_motion[joint][0] + (joint_indexs[joint]*((range_of_motion[joint][1] - range_of_motion[joint][0])/(density -1)))
@@ -66,14 +89,52 @@ class calibration_testing:
                                             up_or_down[(joint)] = not up_or_down[(joint)]
 
                                             
+
                                     time.sleep(1)
-                                    recorded_dvrk_joint_positions.append(self._robot.get_current_joint_position())
-                                    recorded_dvrk_cartesian_positions.append(self._robot.get_current_position().p)
-                                    recorded_atracsys_positions.append( [(self._points[0].x)/10**3, (self._points[0].y)/10**3, (self._points[0].z)/10**3 ]  )
-                                    print up_or_down
+                                    if len(self._points) == 1:
+                                        recorded_dvrk_joint_positions.append(self._robot.get_current_joint_position())
+                                        recorded_dvrk_cartesian_positions.append(self._robot.get_current_position().p)
+                                        recorded_atracsys_positions.append( [(self._points[0].x)/10**3, (self._points[0].y)/10**3, (self._points[0].z)/10**3 ]  )
+                                    elif len(self._points) >= 2:
+                                        print "\n More then one fiducial point found"
+                                        points_skipped = points_skipped +1
+                                        print points_skipped, " points skipped"
+                                    elif len(self._points) == 0:
+                                        print "\n Atracsys fiducial not found"
+                                        points_skipped = points_skipped +1
+                                        print points_skipped, " points skipped"
+
+            sys.stdout.write('\rProgress %02.3f%%' %(100))
+            self._robot.move_joint(numpy.array([0.0,0.0,0.1,0.0,0.0,0.0,-0.20]))
+            #write values to csv file
+            csv_file_name = 'Atracsys_joint_motion_output.csv'
+            print "Values will be saved in: ", csv_file_name
+            f = open(csv_file_name, 'wb')
+            writer = csv.writer(f)
+            writer.writerow(["atracsys positions","","","dvrk cartesian positions", "", "", "dvrk joint positions"])
+            for row in range(len(recorded_atracsys_positions)):
+                writer.writerow([recorded_atracsys_positions[row][0],recorded_atracsys_positions[row][1],recorded_atracsys_positions[row][2],recorded_dvrk_cartesian_positions[row][0],recorded_dvrk_cartesian_positions[row][1],recorded_dvrk_cartesian_positions[row][2],recorded_dvrk_joint_positions[row][0],recorded_dvrk_joint_positions[row][1],recorded_dvrk_joint_positions[row][2],recorded_dvrk_joint_positions[row][3],recorded_dvrk_joint_positions[row][4],recorded_dvrk_joint_positions[row][5],recorded_dvrk_joint_positions[row][6]])
+            
+            #dvrk points to numpy array
+            number_of_points = len(recorded_atracsys_positions)
+            dvrk_coordinates = numpy.zeros(shape=(number_of_points,3))
+            for coordinate in range(len(recorded_dvrk_cartesian_positions)):
+                dvrk_coordinates[coordinate] = [recorded_dvrk_cartesian_positions[coordinate][0], recorded_dvrk_cartesian_positions[coordinate][1], recorded_dvrk_cartesian_positions[coordinate][2]]
+            dvrk_coordinates_for_testing = dvrk_coordinates.astype(float)
+            #atracsys points to numpy array
+            atracsys_coordinates = numpy.zeros(shape=(number_of_points,3))
+            for coordinate in range(len(recorded_atracsys_positions)):
+                atracsys_coordinates[coordinate] = [recorded_atracsys_positions[coordinate][0], recorded_atracsys_positions[coordinate][1], recorded_atracsys_positions[coordinate][2]]
+            atracsys_coordinates_for_testing = atracsys_coordinates.astype(float)
+            #calculate and display transformation and FRE 
+            (transformation, FRE) = nmrRegistrationRigid(dvrk_coordinates_for_testing,atracsys_coordinates_for_testing)
+            print 'dvrk positions: \n', dvrk_coordinates_for_testing
+            print 'atracsys positions: \n', atracsys_coordinates_for_testing
+            print 'Transformation: \n', transformation
+            print 'FRE: \n', FRE
 
 
-
+            rospy.signal_shutdown('Finished Task')
 
 if (len(sys.argv) != 2):
     print sys.argv[0] + ' requires one argument, i.e. name of dVRK arm'
