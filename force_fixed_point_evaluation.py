a5248941f41c6f39a457f4cb3db9c19c8e768e9e
neusman1 <nick.eusman@gmail.com>
2017-02-03 18:37:06 -0500

    changing formAT

---
+++ force_fixed_point_evaluation.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+#Created on Feburary 3, 2017
+#Author: Nick Eusman
+
+import sys
+from dvrk.psm import *
+import rospy
+import csv
+import math
+import numpy
+import PyKDL
+import os
+import time
+import datetime
+
+
+def run():
+
+    ##compute k0,k1,k2 for each point
+    ##output raw into one file and avg in console
+    
+
+
+
+    
+run()
