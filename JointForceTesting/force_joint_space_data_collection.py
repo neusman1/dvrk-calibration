c7e19ddfc79df6a413694ebb6f9f20b93276d4f8
neusman1 <nick.eusman@gmail.com>
2017-02-17 14:09:23 -0500

    updated format easier for evaluation

--- JointForceTesting/force_joint_space_data_collection.py
+++ JointForceTesting/force_joint_space_data_collection.py
@@ -30,55 +30,61 @@ class force_testing:
         avg_current_joint_positions = []
         avg_current_joint_effort = []
         avg_depth = []
+        depth_index = []
         
         while not rospy.is_shutdown():
-            jointUnderTesting = 0;
+            jointUnderTesting = 0; #changable to 0, 1,
+            dX = 0.0005
+            totalSample = 20
             self._robot.move(PyKDL.Vector(0.0, 0.0, -0.09))
             time.sleep(.3)
             raw_input('when surface is in place, hit [enter]')
-
+  
             for depthIndex in range(14):
                 depth = -0.09 - (float(depthIndex) * 0.01)
-                print depth
                 self._robot.move(PyKDL.Vector(0.0, 0.0, depth))
                 time.sleep(1)
                 position_nb = 0
                 isEffortThreshold = False
+                print "depth: ", depth
                 while not isEffortThreshold: #move to 20 positions
                     position_nb += 1
-
                     current_joint_positions = 0.0
                     current_joint_effort = 0.0
                     joint_depth = 0.0
-                    for sample_nb in range(20): #take 20 samples at each position
+                    for sample_nb in range(totalSample): #take 20 samples at each position
                         
                         current_joint_effort += self._robot.get_current_joint_effort()[0]
                         current_joint_positions += self._robot.get_current_joint_position()[0]
                         joint_depth += self._robot.get_current_joint_position()[2]
                                                         
                         time.sleep(.02) #sleep after each sample
-                    avg_current_joint_positions.append( current_joint_positions / 20.0 )
-                    avg_current_joint_effort.append( current_joint_effort / 20.0 )
-                    avg_depth.append( joint_depth / 20.0 )
+                    avg_current_joint_positions.append( current_joint_positions / float(totalSample) )
+                    avg_current_joint_effort.append( current_joint_effort / float(totalSample) )
+                    avg_depth.append( joint_depth / float(totalSample) )
+                    depth_index.append(depthIndex)
                     print 'position recorded: ', position_nb
                     time.sleep(.5) 
                     if abs(avg_current_joint_effort[position_nb - 1]) > 1.2:
                         isEffortThreshold = True
                     else:
-                        self._robot.move(PyKDL.Vector((-0.001 * position_nb), 0.0, depth))
-                        print depth
+                        if jointUnderTesting == 0:
+                            self._robot.move(PyKDL.Vector((-dX * position_nb), 0.0, depth))
+                        elif jointUnderTesting == 1:
+                            self._robot.move(PyKDL.Vector( 0.0, (-dX * position_nb), depth))
                         time.sleep(1)
                     
             self._robot.move(PyKDL.Vector(0.0, 0.0, -0.09))
 
             #write values to csv file
-            csv_file_name = 'ForceTestingDataJointSpace/force_joint_space_data_collection_output_at_joint_' + jointUnderTesting + '_' + ('-'.join(str(x) for x in list(tuple(datetime.datetime.now().timetuple())[:6]))) + '.csv'
+            csv_file_name = 'ForceTestingDataJointSpace/force_joint_space_data_collection_output_at_joint_' + str(jointUnderTesting) + '_' + ('-'.join(str(x) for x in list(tuple(datetime.datetime.now().timetuple())[:6]))) + '.csv'
             print "\n Values will be saved in: ", csv_file_name
             f = open(csv_file_name, 'wb')
             writer = csv.writer(f)
-            writer.writerow(["current Joint positions", "current joint effort", "current depth"])
+            writer.writerow([ "depth index", "current depth", "current Joint positions", "current joint effort" ])
             for row in range(len(avg_current_joint_positions)):         #column number for data in csv file
-                writer.writerow([avg_depth[row],
+                writer.writerow([depth_index[row],
+                                 avg_depth[row],
                                  avg_current_joint_positions[row],
                                  avg_current_joint_effort[row] ])
                                  
